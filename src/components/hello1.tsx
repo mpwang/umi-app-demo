8bb264afbeafd2ce68fab82fc5a69eb1ca95ca2f
Randall Wang <randall.wjz@gmail.com>
2019-10-02 01:44:05 +0800

    do not use default export

--- src/components/hello1.tsx
+++ src/components/hello1.tsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 
-class Hello extends Component {
+export class Hello1 extends Component {
   render () {
     return (
       <div>
@@ -9,5 +9,3 @@ class Hello extends Component {
     )
   }
 }
-
-export default Hello
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import  PokeApi from './poke'
-import Hello from 'components/hello1'
-
+import { Hello1 } from 'components/hello1'
 
 export default function() {
   return (
     <div>
       <PokeApi historyLength={4}/>
-      <Hello />
+      <Hello1 />
     </div>
   )
 }
